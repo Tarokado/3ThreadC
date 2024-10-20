11a14651e5e1e0a154d300f55e01ba9feb671d7a
Tairo <88587168+Tarokado@users.noreply.github.com>
2024-10-20 21:18:36 +0700

    Update THREAD.c

--- THREAD.c
+++ THREAD.c
@@ -3,131 +3,146 @@
 #include <string.h>
 #include <pthread.h>
 #include <unistd.h>
-//Define Max value of data and file path .txt
-#define MAX 256
+//Define file path .txt
 #define FILENAME "thongtinsinhvien.txt"
 // Structure to hold student data
-
-pthread_t threadid1, threadid2, threadid3;
-
 typedef struct {
-    char name[MAX];
-    char dob[MAX];
-    char hometown[MAX];
+    char Name[256];
+    char Date[256];
+    char Home[256];
 } Student;
 // Shared resources
 Student student;
-pthread_mutex_t mutex;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;;
 pthread_cond_t cond_input, cond_write, cond_read;
-int ready_to_write = 0;
-int ready_to_read = 0;
-
+int re_write = 0;
+int re_read = 0;
 // Function to get student data from user
-void read_input(const char *prompt, char *field) {
+void read_input(const char *prompt, char *Stu) {
     printf("%s", prompt);
-    if (fgets(field, MAX, stdin) == NULL) {
-        perror("Error reading input");
+    if (fgets(Stu, 256, stdin) == NULL) {
+        printf("Cant read input");
     }
-    field[strcspn(field, "\n")] = '\0';
+    size_t len = strcspn(Stu, "\n");
+    Stu[len] = '\0';
 }
-// keyboard input
-void get_student_data(Student *student) {
-    read_input("Enter student name: ", student->name);
-    read_input("Enter date of birth: ", student->dob);
-    read_input("Enter hometown: ", student->hometown);
+// keyboard input user data
+void get_student_data(Student *stu) {
+    printf("Enter name: ");
+    scanf("%s", stu->Name);
+    printf("Enter BirthDay: ");
+    scanf("%s", stu->Date);
+    printf("Enter Home: ");
+    scanf("%s", stu->Home);
 }
 // Function to write student data to a file
 void write_data(Student *student) {
     FILE *file = fopen(FILENAME, "a");
-    /*if (file == NULL) {
-        perror("Failed to open file for writing");
+    
+    if (file == NULL) {
+        printf("Failed to open file for writing");
         exit(EXIT_FAILURE);
-    } */
+    } 
     // Write student data to file
-    fprintf(file, "Name: %s\nDOB: %s\nHometown: %s\n",student->name, student->dob, student->hometown);
+    if (fprintf(file, "Name: %s\nBirthDay: %s\nHome: %s\n", student->Name, student->Date, student->Home) < 0) {
+        printf("Failed to write data to file");
+    }    
     fclose(file);
 }
 // Function to read student data from a file
 void read_data() {
-    char buffer[MAX];
+    char buffer[256];
     FILE *file = fopen(FILENAME, "r");
-    /*if (file == NULL) {
-        perror("Failed to open file for reading");
+    if (file == NULL) {
+        printf("Failed to open file for reading");
         exit(EXIT_FAILURE);
-    } */
+    } 
     // Print the entire file contents (all student records)
     printf("\nStudent Inform\n");
-    while (fgets(buffer, MAX, file) != NULL) {
+    while (fgets(buffer, 256, file) != NULL) {
         printf("%s", buffer);
     }
     printf("End of Input\n");
     fclose(file);
 }
 //Thread1 input
-void *input_thread(void *args) {
-    while (1) {
-        pthread_mutex_lock(&mutex);
+static void *input_thread(void *args) {
+    while(1){
+        pthread_mutex_lock(&lock);
         get_student_data(&student); // Get student data
         // Notify write thread
-        ready_to_write = 1;
+        re_write = 1;
         pthread_cond_signal(&cond_write);
         // Wait for reading thread to finish before allowing more input
-        pthread_cond_wait(&cond_input, &mutex);
-        pthread_mutex_unlock(&mutex);
+        pthread_cond_wait(&cond_input, &lock);
+        sleep(5);
+        pthread_mutex_unlock(&lock);
     }
     return NULL;
 }
 //Thread2 write
-void *write_thread(void *args) {
-    while (1) {
-        pthread_mutex_lock(&mutex);
+static void *write_thread(void *args) {
+    while(1){
+        pthread_mutex_lock(&lock);
         // Wait for input thread to provide student data
-        while (!ready_to_write){
-            pthread_cond_wait(&cond_write, &mutex);
+        while (!re_write){
+            pthread_cond_wait(&cond_write, &lock);
         }
         // Write student data
         write_data(&student);
         // Notify read thread
-        ready_to_read = 1;
+        re_read = 1;
+        sleep(1);
         pthread_cond_signal(&cond_read);
-        ready_to_write = 0; // Reset ready to write flag
-        pthread_mutex_unlock(&mutex);
+        re_write = 0; // Reset ready to write flag
+        pthread_mutex_unlock(&lock);
     }
     return NULL;
 }
 //Thread3 read
-void *read_thread(void *args) {
-    while (1) {
-        pthread_mutex_lock(&mutex);
+static void *read_thread(void *args) {
+    while(1){
+        pthread_mutex_lock(&lock);
         // Wait for write thread to finish writing
-        while (!ready_to_read){
-            pthread_cond_wait(&cond_read, &mutex);
+        while (!re_read){
+            pthread_cond_wait(&cond_read, &lock);
         }
         // Read student data
         read_data();
         // Notify input thread to take new input
-        ready_to_read = 0;
+        re_read = 0;
+        sleep(1);
         pthread_cond_signal(&cond_input);
-        pthread_mutex_unlock(&mutex);
+        pthread_mutex_unlock(&lock);
     }
     return NULL;
 }
-int main() {
+int main(int argc, char const *argv[]) {
     // Initialize mutex and condition variables
-    pthread_mutex_init(&mutex, NULL);
-    pthread_cond_init(&cond_input, NULL);
-    pthread_cond_init(&cond_write, NULL);
-    pthread_cond_init(&cond_read, NULL);
-    //Create function
-    pthread_create(&threadid1, NULL, input_thread, NULL);
-    pthread_create(&threadid2, NULL, write_thread, NULL);
-    pthread_create(&threadid3, NULL, read_thread, NULL);
+    if (pthread_mutex_init(&lock, NULL) != 0){
+        printf("Mutex initialization failed.\n");
+        return 1;
+    }
+    if (pthread_cond_init(&cond_input, NULL) != 0 ||
+        pthread_cond_init(&cond_write, NULL) != 0 ||
+        pthread_cond_init(&cond_read, NULL) != 0) {
+        perror("Failed for conditional variables");
+        exit(0);
+    }
+    pthread_t threadid1, threadid2, threadid3;
+    // Create threads
+    if (pthread_create(&threadid1, NULL, input_thread, NULL) != 0 ||
+        pthread_create(&threadid2, NULL, write_thread, NULL) != 0 ||
+        pthread_create(&threadid3, NULL, read_thread, NULL) != 0) {
+        perror("Cant create thread");
+        exit(EXIT_FAILURE);
+    }
     // Wait for threads to finish
     pthread_join(threadid1, NULL);
     pthread_join(threadid2, NULL);
     pthread_join(threadid3, NULL);
-    //Destroy all mutex and cond
-    pthread_mutex_destroy(&mutex);
+    // Destroy all mutex and condition variables
+    pthread_mutex_destroy(&lock);
     pthread_cond_destroy(&cond_input);
     pthread_cond_destroy(&cond_write);
     pthread_cond_destroy(&cond_read);
